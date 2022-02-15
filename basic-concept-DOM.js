699b19768d316aafae409b8fe9ac5c88c2e13010
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-02-15 22:37:53 +0600

    first commit

---
+++ basic-concept-DOM.js
@@ -0,0 +1,22 @@
+//difference between elementGetByID vs elementGetByClassName output.
+//done:
+//elementGetByID return object and elementGetByClassName and id gives array like object. if we would like to access the this array for loop can be used.
+
+//manipulate any css style of an website.
+//done
+//changing the color or the background with a button.
+//done
+//adding element like adding blog
+//done
+//adding a whole blog with a button 
+//done
+//adding comment and with button 
+//done
+//delete button like github
+//
+//adding and delete a commit if clicking over the comment /bubbling effect 
+//make a banking trisections with function.
+//calculation of shopping card.
+//code matching 
+//compleat the jhanker vai's instruction.
+//watch and practice conceptual seasons concept.
